8a14f25073dc68fcdfa46f41569a372e82be3fa7
Martin Coll <mcoll@dc.uba.ar>
2023-11-15 15:44:07 -0300

    seq and par working!

--- chyp/transformer.py
+++ chyp/transformer.py
@@ -27,23 +27,22 @@ class YamlTransformer:
     def transform(self, tokens):
         """documents give rewriting rules"""
         tokens: Iterator[yaml.Token] = iter(tokens)
-        document_name = None
+        document = None
         # the very first rule rewrites 0 to the file name
         while token := next(tokens, None):
             match token:
                 case yaml.StreamEndToken():
-                    return document_name
+                    return
                 case yaml.StreamStartToken():
-                    document_name = self.transform_document(tokens)
+                    document = self.transform_document(tokens)
                 case yaml.DocumentStartToken():
                     meta = Meta(token, token)
                     tokens = itertools.chain([token], tokens)
-                    term = self.term_ref(meta, [document_name])
-                    rw_part_name = self.transform_document(tokens)
-                    rw_term = self.term_ref(meta, [rw_part_name])
+                    rw_term = self.transform_document(tokens)
                     rw_part = self.rewrite_part(meta, [
                          False, [0, 0, rw_term], None])
-                    self.rewrite(meta, [False, document_name, term, rw_part])
+                    rw_name = ""
+                    self.rewrite(meta, [False, rw_name, document, rw_part])
 
     def transform_document(self, tokens: Iterator[yaml.Token]):
         document_name = None
@@ -78,56 +77,51 @@ class YamlTransformer:
                 case yaml.ScalarToken():
                     meta = Meta(token, token)
                     self.gen(meta, [token.value, [('', 1)], [('', 1)], None])
-                    return token.value
+                    term = self.term_ref(meta, [token.value])
+                    return term
 
     def transform_mapping(self, tokens: Iterator[yaml.Token]):
         """parallel keys and key->value rules"""
-        mapping_token, mapping_name = None, None
+        mapping_token, entry = None, None
         while token := next(tokens, None):
             match token:
                 case yaml.BlockEndToken():
-                    return mapping_name
+                    return entry
                 case yaml.BlockMappingStartToken():
                     mapping_token = token
+                    entry = self.transform_mapping_entry(tokens)
                 case yaml.KeyToken():
                     tokens = itertools.chain([token], tokens)
+                    new_entry = self.transform_mapping_entry(tokens)
                     # TODO multiple entries
-                    mapping_name = self.transform_mapping_entry(tokens)
+                    entry = self.par([entry, new_entry])
 
     def transform_mapping_entry(self, tokens: Iterator[yaml.Token]):
-        """a key->value sequential rule"""
-        key_token, key_name = None, None
+        """a key->value sequential rule with key as rule name"""
+        key_token, key = None, None
         while token := next(tokens, None):
             match token:
                 case yaml.KeyToken():
                     key_token = token
-                    key_name = self.transform_object(tokens)
+                    key = self.transform_object(tokens)
                 case yaml.ValueToken():
-                    # key_meta = Meta(key_token, key_token)
-                    # key = self.term_ref(key_meta, [key_name])
-                    value_name = self.transform_object(tokens)
-                    value_meta = Meta(token, token)
-                    value = self.term_ref(value_meta, [value_name]) \
-                            if value_name else self.id0([None])
+                    value = self.transform_object(tokens)
                     meta = Meta(key_token, token)
-                    self.def_statement(meta, [key_name, value, None])
-                    return key_name
+                    return self.seq(meta, [key, value])
 
     def transform_sequence(self, tokens: Iterator[yaml.Token]):
-        """sequential composition of an arbitrary number of objects"""
-        rw_name = None
+        """anonymous sequential composition of an arbitrary number of objects"""
+        start_token = None
+        sequence = None
         while token := next(tokens, None):
             match token:
                 case yaml.BlockEndToken():
-                    meta = Meta(token, token)
-                    self.def_statement(meta, [rw_name, term, None])
-                    return rw_name
+                    return sequence
                 case yaml.BlockSequenceStartToken():
-                    rw_name = self.transform_object(tokens)
-                    meta = Meta(token, token)
-                    term = self.term_ref(meta, [rw_name])
+                    start_token = token
+                    sequence = self.transform_object(tokens)
                 case _:
-                    value_name = self.transform_object(tokens)
-                    meta = Meta(token, token)
-                    value = self.term_ref(meta, [value_name])
-                    term = self.seq(meta, [term, value])
+                    tokens = itertools.chain([token], tokens)
+                    value = self.transform_object(tokens)
+                    meta = Meta(start_token, token)
+                    sequence = self.seq(meta, [sequence, value])
